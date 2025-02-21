c269f34e7dd7d4c5bb552c6c9755e6076de0c3dd
Srikar K <srikarkovvuri5@gmail.com>
2025-02-21 18:25:35 -0500

    initial commit

---
+++ house.py
@@ -0,0 +1,21 @@
+import numpy as numpy
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+df = pd.read_csv('house_prices.csv')
+
+x = df[['Square Footage']]
+y = df['Price']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
+
+model = LinearRegression()
+
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+
+print("Mean squared error: " + mean_squared_error(y_test, y_pred))
+
