3ba1bffd008ff9ad28b540fa33dc0d9e20e1f0c8
Srikar K <srikarkovvuri5@gmail.com>
2025-02-22 17:15:56 -0500

    initial linear reg

--- house.py
+++ house.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
+
 df = pd.read_csv('house_prices.csv')
 
 x = df[['Square Footage']]
